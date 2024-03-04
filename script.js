61b3f4fe4e3efcba4dc4bf8e10551337f5e72616
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2024-03-04 20:59:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,4 @@ const inputs = document.querySelectorAll('.controls input');
 
     inputs.forEach(input => input.addEventListener('change', handleUpdate));
     inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+
